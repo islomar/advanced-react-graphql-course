4306a9fe21e34d690d754e8756749c1ec9b76985
Isidro Lopez <islomar@gmail.com>
2021-02-12 21:25:12 +0100

    Module 2: pass props children to the Page component and render it

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,8 @@
-export default function Page() {
-    return <div>
-        <h2>I am the page component</h2>
+export default function Page(props) {
+  return (
+    <div>
+      <h2>I am the page component</h2>
+      {props.children}
     </div>
+  );
 }
